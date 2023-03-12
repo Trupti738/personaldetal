6ed7a4f157507fd7400e54f6659d70f5d21b0d87
Trupti738 <p.trupti738@gmail.com>
2023-03-12 18:04:44 +0000

    commit1

---
+++ src/personal_detail.java
@@ -0,0 +1,25 @@
+public class personal_detail {
+
+    static String name = "trupti";
+    static int age = 40;
+    static String surname = "patel";
+    static double amount = 90.26;
+    static long MobileNum = 7952771157L;
+    static char grade = 'C';
+    static String address = "829 West street ,London, SW1890A";
+    static int rollNumber = 36;
+    static boolean eligibility = true;
+
+
+    public static void main(String[] args) {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(surname);
+        System.out.println(MobileNum);
+        System.out.println(grade);
+        System.out.println(address);
+        System.out.println(amount);
+        System.out.println(rollNumber);
+        System.out.println(eligibility);
+    }
+}
